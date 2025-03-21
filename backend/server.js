ffd3b4ba600dcf5a0eff677160e62247e5ca2a2b
SaddamTechie <saddamtechie@gmail.com>
2025-03-21 14:53:20 +0300

    configured for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const authRoute = require('./routes/auth');
 const dashboardRoute = require('./routes/dashboard');
 const forumRoute = require('./routes/forum');
 const marketplaceRoute = require('./routes/marketplace');
+const path = require('node:path');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -20,6 +21,15 @@ app.use('/api/auth', authRoute);
 app.use('/api/dashboard', dashboardRoute);
 app.use('/api/forum',forumRoute );
 app.use('/api/marketplace', marketplaceRoute);
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+ 
+    app.get("*",(req,res)=>{
+     res.sendFile(path.resolve(__dirname,"../frontend","dist","index.html"));
+    })
+ }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
